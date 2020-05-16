79b15869042e242f3882a8a324617bfa9b41637b
Martin Majlis <martin@majlis.cz>
2020-05-16 06:47:35 +0200

    Adds packages based on Debian

--- generate.sh
+++ generate.sh
@@ -1,20 +1,47 @@
 #!/usr/bin/env bash
 
-PYTHONS=(\ 
-    # 3.8.2-buster 3.8.2-alpine3.11 3.8.2-alpine3.10 3.8.2-windowsservercore-ltsc2016 3.8.2-windowsservercore-1809\
-    #3.7.7-buster\
+# https://hub.docker.com/_/python
+# alpine - 
+# buster/stretch - https://wiki.debian.org/DebianReleases - Buster (2019-07-06), Stretch (2017-06-17)
+# slim - this image does not contain the common packages => we highly recommend using the default image
+
+PYTHONS=(\
+    latest \3
+    3 \
+    3.8 \
+    3.8.3 \
+    3.8.3-buster \
+    3.8.3-alpine3.11 \
+    3.8.3-alpine3.10 \    
+    3.8.2-buster \
+    3.8.2-alpine3.11 \
+    3.8.2-alpine3.10 \
+    # 3.8.2-windowsservercore-ltsc2016 3.8.2-windowsservercore-1809\
+    3.7\
+    3.7.7\
+    3.7.7-buster\
+    3.7.7-stretch\
     3.7.7-alpine3.11\
-    3.7.7-alpine3.10\ 
+    3.7.7-alpine3.10\
     #3.7.7-windowsservercore-ltsc2016 3.7.7-windowsservercore-1809\
-    #3.6.9-buster\
+    3.6\
+    3.6.9\
+    3.6.9-buster\
+    3.6.9-stretch\
     3.6.9-alpine3.11\
-    3.6.9-alpine3.10\ 
+    3.6.9-alpine3.10\
     #3.6.9-windowsservercore-ltsc2016 3.6.9-windowsservercore-1809\
-    #3.5.9-buster\
+    3.5\
+    3.5.9\
+    3.5.9-buster\
+    3.5.9-stretch\
     3.5.9-alpine3.11\
-    3.5.9-alpine3.10\ 
+    3.5.9-alpine3.10\
     #3.5.9-windowsservercore-ltsc2016 3.5.9-windowsservercore-1809\
-    #2.7.18-buster\
+    2.7\
+    2.7.18\
+    2.7.18-buster\
+    2.7.18-stretch\
     2.7.18-alpine3.11\
     2.7.18-alpine3.10\
 )
@@ -40,17 +67,25 @@ for python_version in ${PYTHONS[@]}; do
     docker_file=${docker_dir}/Dockerfile;
     docker_tag="${python_version}-${scipy_version}";
     mkdir -p ${docker_dir};
-    cat > ${docker_file} <<- EOM
+      cat > ${docker_file} <<- EOM
 FROM python:${python_version}
 
 LABEL python=${python_version}
 LABEL scipy=${scipy_version}
 LABEL numpy=${numpy_version}
-
-RUN apk add --update build-base gcc g++ make bash curl coreutils automake openblas openblas-dev
-RUN pip install --upgrade pip
-RUN pip install scipy==${scipy_version} numpy==${numpy_version}
 EOM
+
+    if [[ $python_version == *"alpine"* ]]; then
+      # install packages needed by alpine
+      echo "RUN apk add --update build-base gcc g++ make bash curl coreutils automake openblas openblas-dev" >> ${docker_file};
+    else
+      # install packages needed by Debian
+      # echo "RUN apt-get install build-base gcc g++ make bash curl coreutils automake openblas openblas-dev" >> ${docker_file};
+      cat /dev/null;
+    fi;
+
+    echo "RUN pip install scipy==${scipy_version} numpy==${numpy_version}" >> ${docker_file};    
+
     echo "File: ${docker_file}; Tag: ${docker_tag}";    
   done;
 done
