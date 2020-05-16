2320509675f5e36ffc2b338dd2a5ddd65aa2a8de
Martin Majlis <martin@majlis.cz>
2020-05-16 07:47:36 +0200

    Create tags for releases

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:3.6.9-stretch
+FROM python:latest
 
-LABEL python=3.6.9-stretch
-LABEL scipy=1.3.1
-LABEL numpy=1.17.0
-RUN pip install scipy==1.3.1 numpy==1.17.0
+LABEL python=latest
+LABEL scipy=1.3.3
+LABEL numpy=1.18.2
+RUN pip install scipy==1.3.3 numpy==1.18.2
--- generate.sh
+++ generate.sh
@@ -6,44 +6,44 @@
 # slim - this image does not contain the common packages => we highly recommend using the default image
 
 PYTHONS=(\
-    latest \3
-    3 \
-    3.8 \
-    3.8.3 \
-    3.8.3-buster \
-    3.8.3-alpine3.11 \
-    3.8.3-alpine3.10 \    
-    3.8.2-buster \
-    3.8.2-alpine3.11 \
-    3.8.2-alpine3.10 \
-    # 3.8.2-windowsservercore-ltsc2016 3.8.2-windowsservercore-1809\
-    3.7\
-    3.7.7\
-    3.7.7-buster\
-    3.7.7-stretch\
-    3.7.7-alpine3.11\
-    3.7.7-alpine3.10\
-    #3.7.7-windowsservercore-ltsc2016 3.7.7-windowsservercore-1809\
-    3.6\
-    3.6.9\
-    3.6.9-buster\
-    3.6.9-stretch\
-    3.6.9-alpine3.11\
-    3.6.9-alpine3.10\
-    #3.6.9-windowsservercore-ltsc2016 3.6.9-windowsservercore-1809\
-    3.5\
-    3.5.9\
-    3.5.9-buster\
-    3.5.9-stretch\
-    3.5.9-alpine3.11\
-    3.5.9-alpine3.10\
-    #3.5.9-windowsservercore-ltsc2016 3.5.9-windowsservercore-1809\
-    2.7\
-    2.7.18\
-    2.7.18-buster\
-    2.7.18-stretch\
-    2.7.18-alpine3.11\
     2.7.18-alpine3.10\
+    2.7.18-alpine3.11\
+    2.7.18-stretch\
+    2.7.18-buster\
+    2.7.18\
+    2.7\
+    #3.5.9-windowsservercore-ltsc2016 3.5.9-windowsservercore-1809\
+    3.5.9-alpine3.10\
+    3.5.9-alpine3.11\
+    3.5.9-stretch\
+    3.5.9-buster\
+    3.5.9\
+    3.5\
+    #3.6.9-windowsservercore-ltsc2016 3.6.9-windowsservercore-1809\
+    3.6.9-alpine3.10\
+    3.6.9-alpine3.11\
+    3.6.9-stretch\
+    3.6.9-buster\
+    3.6.9\
+    3.6\
+    #3.7.7-windowsservercore-ltsc2016 3.7.7-windowsservercore-1809\
+    3.7.7-alpine3.10\
+    3.7.7-alpine3.11\
+    3.7.7-stretch\
+    3.7.7-buster\
+    3.7.7\
+    3.7\
+    # 3.8.2-windowsservercore-ltsc2016 3.8.2-windowsservercore-1809\
+    3.8.2-alpine3.10 \
+    3.8.2-alpine3.11 \
+    3.8.2-buster \
+    3.8.3-alpine3.10 \    
+    3.8.3-alpine3.11 \
+    3.8.3-buster \
+    3.8.3 \
+    3.8 \
+    3 \
+    latest\
 )
 
 # 1.4.1 cannot be built
@@ -86,6 +86,16 @@ EOM
 
     echo "RUN pip install scipy==${scipy_version} numpy==${numpy_version}" >> ${docker_file};    
 
-    echo "File: ${docker_file}; Tag: ${docker_tag}";    
+    echo "File: ${docker_file}; Tag: ${docker_tag}";
+
+    cp ${docker_file} Dockerfile
+    git add Dockerfile ${docker_file}
+    git tag -d ${docker_tag}
+    git push --delete origin ${docker_tag}
+    git commit -m "Generating docker for ${docker_tag}" ${docker_file} Dockerfile
+    git tag -a ${docker_tag} -m ${docker_tag}
   done;
 done
+git push origin
+git push origin --tags
+
